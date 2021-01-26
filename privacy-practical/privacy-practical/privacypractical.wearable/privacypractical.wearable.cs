e15078cdf3b8f12255707016d01919254681df68
Warin <chris.warin1@gmail.com>
2021-01-26 22:29:10 +0100

    .NET Template

---
+++ privacy-practical/privacy-practical/privacypractical.wearable/privacypractical.wearable.cs
@@ -0,0 +1,22 @@
+using System;
+using privacypractical.lib;
+
+namespace privacypractical.wearable
+{
+    class Program : global::Xamarin.Forms.Platform.Tizen.FormsApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            
+            LoadApplication(new App());
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            global::Xamarin.Forms.Platform.Tizen.Forms.Init(app);
+            app.Run(args);
+        }
+    }
+}
